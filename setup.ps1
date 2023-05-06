41482438c405ac1dce49db13692477eee35c9e1c
Sarvex <sarvex@jatasra.com>
2023-05-06 14:06:32 +0530

    Added Sublime

--- setup.ps1
+++ setup.ps1
@@ -12,7 +12,7 @@ scoop install git
 scoop bucket known | ForEach-Object{ scoop bucket add $_ }
 scoop bucket add sarvex https://github.com/sarvex/scoop
 scoop install sudo aria2 curl grep sed less touch search-multisource terminal-icons oh-my-posh ripgrep fd llvm emacs python julia go make cmake premake 
-scoop install CascadiaCode AnonymousPro-NF BitstreamVeraSansMono-NF CascadiaCode-NF CodeNewRoman-NF Cousine-NF DejaVuSansMono-NF DroidSansMono-NF FantasqueSansMono-NF FiraCode-NF Hack-NF Hasklig-NF Inconsolata-NF InconsolataGo-NF Iosevka-NF JetBrainsMono-NF LiberationMono-NF Meslo-NF Overpass-NF Noto-NF ProggyClean-NF RobotoMono-NF SourceCodePro-NF SpaceMono-NF Terminus-NF UbuntuMono-NF VictorMono-NF
+scoop install AurulentSansMono-NF Agave-NF AnonymousPro-NF BitstreamVeraSansMono-NF CascadiaCode CascadiaCode-NF CodeNewRoman-NF ComicShannsMono-NF Cousine-NF DejaVuSansMono-NF DroidSansMono-NF FantasqueSansMono-NF FiraCode-NF Hack-NF Hasklig-NF Inconsolata-NF InconsolataGo-NF Iosevka-NF IosevkaTerm-NF JetBrainsMono-NF LiberationMono-NF Meslo-NF Overpass-NF Noto-NF ProggyClean-NF RobotoMono-NF SourceCodePro-NF SpaceMono-NF Terminus-NF UbuntuMono-NF VictorMono-NF
 
 # Install all apps
 winget import --import-file "$dotfiles\winget.apps.json" --disable-interactivity
