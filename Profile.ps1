b7c97be5e729baa4a9145ebab0def54f62622d43
Sarvex ☠ Jatasra <sarvex.jatasra@gmail.com>
2023-06-09 08:03:09 +0530

    Added LunarVim

--- Profile.ps1
+++ Profile.ps1
@@ -1,3 +1,5 @@
 Import-Module Terminal-Icons
 Invoke-Expression (&scoop-search-multisource.exe --hook)
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/capr4n.omp.json" | Invoke-Expression
+
+Set-Alias lvim 'C:\Users\Yoga\.local\bin\lvim.ps1'
