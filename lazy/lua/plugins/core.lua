b29c1737e46ffcbdf21783b076278ba32c1d5435
Sarvex ☠ Jatasra <sarvex.jatasra@gmail.com>
2023-07-21 21:13:09 +0530

    Initial commit

--- lazy/lua/plugins/core.lua
+++ lazy/lua/plugins/core.lua
@@ -1,27 +1,4 @@
 return {
-
-  -- {
-  --   'xiyaowong/transparent.nvim',
-  --   opts = {
-  --     groups = { -- table: default groups
-  --       'Normal', 'NormalNC', 'Comment', 'Constant', 'Special', 'Identifier', 'Statement', 'PreProc', 'Type',
-  --       'Underlined', 'Todo', 'String', 'Function', 'Conditional', 'Repeat', 'Operator', 'Structure', 'LineNr',
-  --       'NonText', 'SignColumn', 'CursorLineNr', 'EndOfBuffer',
-  --     },
-  --     extra_groups = {
-  --       'NormalFloat',
-  --       'NvimTreeNormal',
-  --       'NvimTreeNormalNC',
-  --       'NvimTreeNormalFloat',
-  --       'NvimTreeEndOfBuffer',
-  --     },                   -- table: additional groups that should be cleared
-  --     exclude_groups = {}, -- table: groups you don't want to clear
-  --   },
-  --   keys = {
-  --     { '<leader>uT', '<cmd>TransparentToggle<CR>', desc = 'Toggle transparency' },
-  --   },
-  -- },
-
   {
     'folke/trouble.nvim',
     -- opts will be merged with the parent spec
